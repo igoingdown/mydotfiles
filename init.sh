f285320bbf421f5c587c83a4e51f369b9e7be0cb
igoingdown <fycjmingxing@126.com>
2020-02-17 12:03:59 +0800

    [feat] common init on master branch

---
+++ init.sh
@@ -0,0 +1,79 @@
+#/bin/bash
+
+
+#=============== load common funcs ===========================================
+source common_init_funcs.sh
+
+
+# TODO: update source list
+# include pip source and apt-get source
+
+
+#=============== generate ssh key and upload it to github ===========================================
+ssh-keygen
+# TODO: upload public key to github, gitlab and so on or use http first
+
+
+#=============== install zsh  ===========================================
+installZsh
+
+
+#=============== install vim  ===========================================
+sudo apt-get install vim
+vim --version
+
+
+#=============== install vundle  ========================================
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+#=============== install tmux =================================
+sudo apt-get install tmux
+
+
+#=============== tmux, zsh, bash and vim config ============================================
+dragConfFromGithub
+
+
+#=============== install nmap =================================
+sudo apt-get install nmap 
+
+
+#=============== install gcc and cmake  ============================================
+sudo apt update
+sudo apt install build-essential
+sudo apt-get install manpages-dev
+sudo apt-get install cmake
+
+
+#=============== install golang =========================================
+installGo go1.12.7.linux-amd64.tar.gz
+
+
+#=============== install cool projects =========================================
+# install some cool projects that I should learn through
+go get -d k8s.io/kubernetes
+cd $GOPATH/src/k8s.io/kubernetes
+make
+
+
+#=============== Install GDB ============================================
+sudo apt-get update
+sudo apt-get install gdb
+
+
+#=============== Install dlv ============================================
+installDlv
+
+
+#=============== install cool projects =========================================
+# install some cool projects that I should learn through
+installCoolProjects
+
+
+#=============== install YCM  =========================================
+installYCM
+
+
+#=============== install PB =========================================
+installPB
