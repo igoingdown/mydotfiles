c94452bacf1b27d9ab198448b166556e7df25fa7
igoingdown <fycjmingxing@126.com>
2022-02-24 14:30:17 +0800

    [fix]添加 mac 上配置到 dev

--- my_shell_config.sh
+++ my_shell_config.sh
@@ -169,6 +169,16 @@ alias fcid="echo -n 6214830155051807 | tee >(pbcopy)"
 alias pnum="echo -n 18810860130 | tee >(pbcopy)"
 alias intro="echo -n '我是赵明星-北邮-硕-19届-字节后端，因拿到微观博易的 offer 获得了内推资格；目前与安贤量化建立了长期内推合作；家人在微软 bing multimedia 组做nlp 算法工程师'| tee >(pbcopy)"
 alias ref="cd $HOME/github/referral/004-platforms/001-byr-bbs/003-bytedance/002-bbs-raw && cat text.txt | pbcopy && goto bbs"
+alias msref="cd $HOME/github/referral/004-platforms/001-byr-bbs/002-ms-stca/002-bbs-raw && cat text.txt | pbcopy && goto bbs"
+alias mapref="cd $HOME/github/referral/004-platforms/001-byr-bbs/004-amap/002-bbs-raw && cat text.txt | pbcopy && goto bbs"
+alias raref="cd $HOME/github/referral/004-platforms/001-byr-bbs/005-msra/002-bbs-raw && cat text.txt | pbcopy && goto bbs"
+alias wgbyref="cd $HOME/github/referral/004-platforms/001-byr-bbs/007-weiguanboyi/002-bbs-raw && cat text.txt | pbcopy && goto bbs"
+alias axref="cd $HOME/github/referral/004-platforms/001-byr-bbs/008-anxianlianghua/002-bbs-raw && cat text.txt | pbcopy && goto bbs"
+alias hi="cd $HOME/github/referral/002-wechat-hello && cat hello.md | pbcopy"
+alias gkb="ginkgo bootstrap"
+alias gkg="ginkgo generate"
+alias gink="gkb && gkg"
+alias aga='apply-git-acl'
 
 
 #=============== Common Function Setting =============================================
