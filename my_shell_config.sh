0ad610dcd3cf2b7eec5066e7eef008bdda3ac28d
igoingdown <fycjmingxing@126.com>
2022-03-29 15:05:34 +0800

    [fix]添加和拆分配置

--- my_shell_config.sh
+++ my_shell_config.sh
@@ -1,4 +1,4 @@
-#bin/zsh
+#!/bin/bash
 
 
 #=============== Base PATH Setting =============================================
@@ -196,16 +196,6 @@ alias eid="echo -n 8152681 | tee >(pbcopy)"
 alias alarmid="echo -n 6796652467007258628 | tee >(pbcopy)"
 alias fcid="echo -n 6214830155051807 | tee >(pbcopy)"
 alias pnum="echo -n 18810860130 | tee >(pbcopy)"
-alias intro="echo -n '我是赵明星-北邮-硕-19届-字节后端，因拿到微观博易的 offer 获得了内推资格；目前与安贤量化建立了长期内推合作；家人在微软 bing multimedia 组做nlp 算法工程师'| tee >(pbcopy)"
-
-alias thank="echo -n '感谢你的信任！有消息我会及时同步，有问题随时沟通。有其他像你一样优秀的同学需要内推也欢迎联系！'| tee >(pbcopy)"
-alias ref="cd $HOME/github/referral/004-platforms/001-byr-bbs/003-bytedance/002-bbs-raw && ./build.sh &&  goto bbs"
-alias msref="cd $HOME/github/referral/004-platforms/001-byr-bbs/002-ms-stca/002-bbs-raw && ./build.sh && goto bbs"
-alias mapref="cd $HOME/github/referral/004-platforms/001-byr-bbs/004-amap/002-bbs-raw && ./build.sh && goto bbs"
-alias raref="cd $HOME/github/referral/004-platforms/001-byr-bbs/005-msra/002-bbs-raw && ./build.sh && goto bbs"
-alias wgbyref="cd $HOME/github/referral/004-platforms/001-byr-bbs/007-weiguanboyi/002-bbs-raw && ./build.sh && goto bbs"
-alias axref="cd $HOME/github/referral/004-platforms/001-byr-bbs/008-anxianlianghua/002-bbs-raw && ./build.sh && goto bbs"
-alias hi="cd $HOME/github/referral/002-wechat-hello && cat hello.md | pbcopy"
 alias gkb="ginkgo bootstrap"
 alias gkg="ginkgo generate"
 alias gink="gkb && gkg"
@@ -302,6 +292,9 @@ alias dorm='docker rm'
 alias ag='ag --ignore-dir thrift_gen --ignore-dir clients --ignore-dir kitex_gen --ignore-dir pb_gen --ignore-dir ugc_thecat_pyrpc'
 
 
+
+#=============== BYR BBS Setting =============================================
+source $HOME/github/mydotfiles/bbs_conf.sh
 
 
 #=============== ppe shell config =============================================
